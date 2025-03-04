fa33cbe0131a44133f7c9b851592c4098350906e
talyaAmar <seminar@DOMAIN.ORG>
2025-03-04 10:38:22 +0200

    10:38

---
+++ WebApplication1/Women.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1
+{
+    public class Women
+    {
+        public int Id { get; set; }
+        public   string Name { get; set; }
+        public int Age { get; set; }
+        public   string Cors { get; set; }
+
+        public Women(int id,string name,int age,string cors) { 
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+            this.Cors = cors;
+        }
+
+    }
+}
